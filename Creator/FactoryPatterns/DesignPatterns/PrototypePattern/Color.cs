6b48bb36300c2034e410a2c75d819f2a9eb51a2d
zhuchunqing <zhuchunqing@hfitech.com>
2022-04-13 00:32:30 +0800

    原型模式

---
+++ Creator/FactoryPatterns/DesignPatterns/PrototypePattern/Color.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class Color : ColorPrototype
+    {
+        int red;
+        int green;
+        int blue;
+        // Constructor
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+        // Create a shallow copy
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine(
+                "Cloning color RGB: {0,3},{1,3},{2,3}",
+                red, green, blue);
+            return this.MemberwiseClone() as ColorPrototype;
+        }
+    }
+}
---
+++ Creator/FactoryPatterns/DesignPatterns/PrototypePattern/ColorManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class ColorManager
+    {
+        private Dictionary<string, ColorPrototype> colors =
+            new Dictionary<string, ColorPrototype>();
+        // Indexer
+        public ColorPrototype this[string key]
+        {
+            get { return colors[key]; }
+            set { colors.Add(key, value); }
+        }
+    }
+}
